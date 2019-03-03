80a108647cdfaf6d5fd2864d96b34655d52bf7c5
Konab <konab-job@yandex.ru>
2019-03-03 19:57:01 +0300

    Add comments

--- geocoder_51c.py
+++ geocoder_51c.py
@@ -21,6 +21,18 @@ class Geocoder_51c:
 			return response.read()
 
 	def get_address_point(self, n_p, street, house):
+		'''get_address_point
+
+		Returns the geo-point of the address
+		
+		Arguments:
+			n_p {str} -- name of the settlement
+			street {str} -- name of the street
+			house {str} -- house number
+		
+		Returns:
+			[json] -- Geo-data
+		'''
 		address = n_p + ' ' + street + ' ' + house
 		data = self.url + 'get_address_point' + '?' + urlencode({'address': address})
 		with urlopen(data) as response:
@@ -31,6 +43,14 @@ class Geocoder_51c:
 			return result
 
 	def get_diapason(self, point_1, point_2):
+		'''get_diapason
+		
+		Checks if point №2 is within the range of 300 meters from point №1
+		
+		Arguments:
+			point_1 {str} -- geo-data
+			point_2 {str} -- geo-data
+		'''
 		values = {'point_1': 'Point({} {})'.format(point_1[1], point_1[0]), 'point_2': 'Point({} {})'.format(point_2[1], point_2[0])}
 		data = self.url + 'distance' + '?' + urlencode(values)
 		with urlopen(data) as response:
@@ -38,6 +58,18 @@ class Geocoder_51c:
 			return(result)
 
 	def get_closest_point_on_road(self, n_p, street, house):
+		'''get_closest_point_on_road
+		
+		Returns the projection of the building to the road
+		
+		Arguments:
+			n_p {str} -- name of the settlement
+			street {str} -- name of the street
+			house {str} -- house number
+		
+		Returns:
+			[json] -- Geo-data
+		'''
 		address = n_p + ' ' + street + ' ' + house
 		road = n_p + ' ' + street
 		data = self.url + 'closest_point_on_road' + '?' + urlencode({'address': address, 'road': road})
@@ -45,6 +77,19 @@ class Geocoder_51c:
 			return response.read()
 
 	def get_correct_point(self, n_p, street, house, dtp_point):
+		'''get_closest_point_on_road
+		
+		Returns the calculated point
+		
+		Arguments:
+			n_p {str} -- name of the settlement
+			street {str} -- name of the street
+			house {str} -- house number
+			dtp_point {str} -- geo-point
+		
+		Returns:
+			[json] -- Geo-data
+		'''
 		# address = n_p + ' ' + street + ' ' + house
 		# road = n_p + ' ' + street
 		point = 'POINT({} {})'.format(dtp_point[1], dtp_point[0])
